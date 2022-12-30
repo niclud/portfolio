1b67c74a05c787519811c5dd572661d0890bf438
Nicolas Ludueña <nicolascluduena@gmail.com>
2022-12-29 21:35:50 -0300

    medio portfolio hecho

---
+++ src/layouts/Footer.svelte
@@ -0,0 +1,35 @@
+<footer class="relative bg-white px-20 h-16">
+	<div class="grid grid-flow-col items-center h-full">
+		<p>Copyright © 2022. Nicolas Ludueña</p>
+		<div class="justify-self-end grid grid-flow-col gap-4 text-2xl">
+			<a
+				class="uppercase font-semibold transition-all opacity-80 hover:opacity-100"
+				href="/#"
+				target="_BLANK"
+			>
+				<i class="fa-brands fa-whatsapp" />
+			</a>
+			<a
+				class="uppercase font-semibold transition-all opacity-80 hover:opacity-100"
+				href="https://www.instagram.com/nicolasluduena7/"
+				target="__blank"
+			>
+				<i class="fa-brands fa-instagram" />
+			</a>
+			<a
+				class="uppercase font-semibold transition-all opacity-80 hover:opacity-100"
+				href="https://www.linkedin.com/in/nicolas-ludue%C3%B1a-aba674239/"
+				target="__blank"
+			>
+				<i class="fa-brands fa-linkedin" />
+			</a>
+			<a
+				class="uppercase font-semibold transition-all opacity-80 hover:opacity-100"
+				href="mailto:nicolascluduena@gmail.com"
+				target="__blank"
+			>
+				<i class="fa-regular fa-envelope" />
+			</a>
+		</div>
+	</div>
+</footer>
