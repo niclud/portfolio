4dab1fa92f58934ae8b34ad293fb090d51ab5a66
Nicolas Ludueña <nicolascluduena@gmail.com>
2023-01-04 17:42:31 -0300

    Updates

---
+++ _app/immutable/chunks/0-2330d441.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-8633c802.js";export{t as component};
---
+++ _app/immutable/chunks/1-7b484e4c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-f3285d42.js";export{t as component};
